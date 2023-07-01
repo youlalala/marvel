5d6bbfbc8bf11c97073dbd218b164472db41123c
youlalala <reyoula@gmail.com>
2023-07-02 02:50:02 +0900

    :sparkles: Feat: Add Thumbnail Image for Characters Recyclerview

--- feature/characters/src/main/java/com/youlalala/marvel/feature/characters/binding/ViewBinding.kt
+++ feature/characters/src/main/java/com/youlalala/marvel/feature/characters/binding/ViewBinding.kt
@@ -1,7 +1,9 @@
 package com.youlalala.marvel.feature.characters.binding
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.youlalala.marvel.domain.model.Character
 import com.youlalala.marvel.feature.characters.CharactersAdapter
 
@@ -9,4 +11,11 @@ import com.youlalala.marvel.feature.characters.CharactersAdapter
 fun bindCharacters(recyclerView: RecyclerView, itemList: List<Character>){
     val adapter = recyclerView.adapter as CharactersAdapter
     adapter.submitList(itemList)
+}
+
+@BindingAdapter("image_url")
+fun bindImg(view: ImageView, url: String){
+    Glide.with(view.rootView)
+        .load(url)
+        .into(view)
 }
