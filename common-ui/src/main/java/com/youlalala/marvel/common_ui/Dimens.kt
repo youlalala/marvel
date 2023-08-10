b36ea717aa585d8a76250f0c95055251e6ec1251
youlalala <reyoula@gmail.com>
2023-08-11 01:56:26 +0900

    :sparkles: Feat: [Compose] Add Dimensions in common-ui for compose

---
+++ common-ui/src/main/java/com/youlalala/marvel/common_ui/Dimens.kt
@@ -0,0 +1,11 @@
+package com.youlalala.marvel.common_ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
+
+object Dimens {
+
+    val PaddingNormal: Dp
+        @Composable get() = dimensionResource(R.dimen.margin_normal)
+}
