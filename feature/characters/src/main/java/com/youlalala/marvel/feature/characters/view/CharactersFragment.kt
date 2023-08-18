a8fafbcfff6daf38816faef2b6caa430b2a90e5e
youlalala <reyoula@gmail.com>
2023-08-18 23:12:51 +0900

    :sparkles: Add CharactersUiState for loading

--- feature/characters/src/main/java/com/youlalala/marvel/feature/characters/view/CharactersFragment.kt
+++ feature/characters/src/main/java/com/youlalala/marvel/feature/characters/view/CharactersFragment.kt
@@ -6,10 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.youlalala.marvel.feature.characters.CharactersAdapter
 import com.youlalala.marvel.feature.characters.databinding.FragmentCharactersBinding
+import com.youlalala.marvel.feature.characters.viewmodels.CharactersUiState
 import com.youlalala.marvel.feature.characters.viewmodels.CharactersViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
@@ -18,6 +23,8 @@ class CharactersFragment : Fragment() {
     private lateinit var binding: FragmentCharactersBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        observeUiState()
     }
 
     override fun onCreateView(
@@ -32,11 +39,25 @@ class CharactersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply{
-            lifecycleOwner=this@CharactersFragment
+            lifecycleOwner=viewLifecycleOwner
             adapter = CharactersAdapter()
             viewModel = charactersViewModel
         }
 
         charactersViewModel.getCharacters()
+    }
+
+    private fun observeUiState(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                charactersViewModel.charactersUiState.collect { uiState ->
+                    when (uiState) {
+                        is CharactersUiState.Success -> binding.progressBar.visibility = View.GONE
+                        is CharactersUiState.Error -> binding.progressBar.visibility = View.GONE
+                        is CharactersUiState.Loading -> binding.progressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
     }
 }
--- feature/characters/src/main/java/com/youlalala/marvel/feature/characters/viewmodels/CharactersViewModel.kt
+++ feature/characters/src/main/java/com/youlalala/marvel/feature/characters/viewmodels/CharactersViewModel.kt
@@ -7,7 +7,9 @@ import com.youlalala.marvel.domain.usecase.GetCharactersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,14 +18,26 @@ class CharactersViewModel @Inject constructor(
     private val getCharactersUseCase: GetCharactersUseCase
 ): ViewModel(){
 
-    private val _charactersListFlow = MutableStateFlow<List<Character>>(emptyList())
-    val charactersListFlow:StateFlow<List<Character>> = _charactersListFlow
+    private val _charactersUiState = MutableStateFlow<CharactersUiState>(CharactersUiState.Loading)
+    val charactersUiState:StateFlow<CharactersUiState> = _charactersUiState.asStateFlow()
+
 
     fun getCharacters(){
         viewModelScope.launch{
-            getCharactersUseCase().collectLatest{
-                _charactersListFlow.value = it
+            getCharactersUseCase()
+                .onStart {
+                    _charactersUiState.value = CharactersUiState.Loading
+                }.catch {
+                    _charactersUiState.value = CharactersUiState.Error
+                }.collect {
+                    _charactersUiState.value = CharactersUiState.Success(it)
             }
         }
     }
+}
+
+sealed class CharactersUiState(val data: List<Character>?= null){
+    object Loading: CharactersUiState()
+    object Error: CharactersUiState()
+    data class Success(val charactersList: List<Character>): CharactersUiState(data = charactersList)
 }
