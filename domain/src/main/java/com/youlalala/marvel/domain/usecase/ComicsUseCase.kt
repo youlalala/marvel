f095e24ddd3531670021d185a2f3d099f39fbbba
youlalala <reyoula@gmail.com>
2023-08-22 01:23:58 +0900

    :sparkles: add comics feature error handling in domain layer

--- domain/src/main/java/com/youlalala/marvel/domain/usecase/ComicsUseCase.kt
+++ domain/src/main/java/com/youlalala/marvel/domain/usecase/ComicsUseCase.kt
@@ -1,10 +1,12 @@
 package com.youlalala.marvel.domain.usecase
 
-import com.youlalala.marvel.domain.model.Comic
+import com.youlalala.marvel.domain.model.ComicList
 import kotlinx.coroutines.flow.Flow
 
 interface ComicsUseCase {
 
-    fun getComics(): Flow<List<Comic>>
+//    fun getComics(): Flow<List<Comic>>
+
+    fun getComics(): Flow<Result<ComicList>>
 
 }
