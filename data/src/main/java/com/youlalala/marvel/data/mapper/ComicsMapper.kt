17cad387e8072c9ed2ff3afaad7e2740765395ba
youlalala <reyoula@gmail.com>
2023-08-25 01:39:12 +0900

    :recycle: error handling

--- data/src/main/java/com/youlalala/marvel/data/mapper/ComicsMapper.kt
+++ data/src/main/java/com/youlalala/marvel/data/mapper/ComicsMapper.kt
@@ -3,35 +3,19 @@ package com.youlalala.marvel.data.mapper
 import com.youlalala.marvel.data.dto.ComicData
 import com.youlalala.marvel.domain.model.Comic
 
-//object ComicsMapper {
-//
-//    fun toDomain(data: List<ComicData>): List<Comic>{
-//        return data.map {
-//            Comic(
-//                id = it.id,
-//                title = it.title,
-//                thumbnail_url = it.thumbnail.path+"."+it.thumbnail.extension
-//            )
-//        }
-//    }
-//}
-//
-//fun List<ComicData>.toDomain() : List<Comic> {
-//    return ComicsMapper.toDomain(this)
-//}
-//
 object ComicsMapper {
 
-    fun toDomain(data: ComicData): Comic{
-        return Comic(
-                id = data.id,
-                title = data.title,
-                thumbnail_url = data.thumbnail.path+"."+data.thumbnail.extension
+    fun toDomain(data: List<ComicData>): List<Comic>{
+        return data.map {
+            Comic(
+                id = it.id,
+                title = it.title,
+                thumbnail_url = it.thumbnail.path+"."+it.thumbnail.extension
             )
+        }
     }
 }
 
-fun ComicData.toDomain() : Comic {
+fun List<ComicData>.toDomain() : List<Comic> {
     return ComicsMapper.toDomain(this)
 }
-
